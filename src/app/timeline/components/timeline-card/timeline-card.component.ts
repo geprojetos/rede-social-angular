7415f12ad7e6746f72ecf024cacbd5a2aac9b832
geprojetos <geprojetosbrasil@gmail.com>
2019-03-18 16:14:00 -0300

    Criando animação para a busca no efeito de toggle

--- src/app/timeline/components/timeline-card/timeline-card.component.ts
+++ src/app/timeline/components/timeline-card/timeline-card.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { TimelineCardInterface } from '../../interfaces/timeline-card/timeline-card-interface';
-import { TimelineSearchService } from '../../services/timeline-search/timeline-search.service';
-import { Observable } from 'rxjs';
 
 
 @Component({
@@ -14,31 +12,17 @@ import { Observable } from 'rxjs';
 export class TimelineCardComponent implements OnInit {
 
   cards: TimelineCardInterface[] = [];
-  menu$: Observable<boolean>;
-  show: boolean;
 
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _searchService: TimelineSearchService
   ) { }
 
   ngOnInit() {
 
     this._activatedRoute.params.subscribe(() => this.cards = this._activatedRoute.snapshot.data['cards']);
-
-    this.menu$ = this._searchService.menuObservable();
-
-    this.menu$.subscribe(res => {
-      console.log('recebendo do servico');
-      this.show = res;
-      console.log(this.show);
-    })
   };
  
-
-  
-
-  newListCards(newCards) {
+  newListCards(newCards): void {
 
     this.cards = newCards;
   };
--- src/app/timeline/components/timeline-search/timeline-search.component.ts
+++ src/app/timeline/components/timeline-search/timeline-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TimelineSearchService } from '../../services/timeline-search/timeline-search.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-timeline-search',
@@ -9,14 +10,24 @@ import { TimelineSearchService } from '../../services/timeline-search/timeline-s
 })
 export class TimelineSearchComponent implements OnInit {
 
+  menu$: Observable<boolean>;
+  menuToggle: boolean;
+
   constructor(
     private _searchService: TimelineSearchService
   ) { }
 
   ngOnInit() {
+
+    this.menu$ = this._searchService.menuObservable();
+    this.menu$.subscribe(res => {
+
+      console.log(res);
+      this.menuToggle = res;
+    })
   }
 
-  closeSearch(e: Event) {
+  closeSearch(e: Event): void {
 
     e.preventDefault();
     this._searchService.toggleMenu();
