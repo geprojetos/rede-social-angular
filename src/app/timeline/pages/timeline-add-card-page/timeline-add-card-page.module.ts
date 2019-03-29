8f5a531a3df77695e6ec518f56a6d4de2f140baa
geprojetos <geprojetosbrasil@gmail.com>
2019-03-29 19:08:27 -0300

    Criando o formulário para cadastro de cards

--- src/app/timeline/pages/timeline-add-card-page/timeline-add-card-page.module.ts
+++ src/app/timeline/pages/timeline-add-card-page/timeline-add-card-page.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { TimelineAddCardPageComponent } from './timeline-add-card-page.component';
 import { TimelineAddCardPageRoutingModule } from './timeline-add-card-page.routing.module';
+import { NavBackButtonModule } from 'src/app/shared/components/nav-back-button/nav-back-button.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    NavBackButtonModule,
     TimelineAddCardPageRoutingModule
   ],
   declarations: [
