a91067b49150af5df8f3fa2524afaea8be76dcaf
geprojetos <geprojetosbrasil@gmail.com>
2019-05-31 15:29:01 -0300

    Criando estilo para o componente de like

--- src/app/timeline/components/timeline-like/timeline-like.component.ts
+++ src/app/timeline/components/timeline-like/timeline-like.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-timeline-like',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineLikeComponent implements OnInit {
 
+  @Input() likes: number = 0;
+  
   constructor() { }
 
   ngOnInit() {
